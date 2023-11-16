a348637b9277108dc62683639b2d62c004d34c99
Alberto <albertogom.alon@gmail.com>
2023-11-16 17:52:57 +0100

    Primer Commit

--- Frontend/Vue/src/store/home.module.ts
+++ Frontend/Vue/src/store/home.module.ts
@@ -1,4 +1,3 @@
-import { TagsService, ArticlesService } from "../common/api.service";
 import { FETCH_ARTICLES, FETCH_TAGS } from "./actions.type";
 import {
   FETCH_START,
@@ -26,28 +25,6 @@ const getters = {
   },
   tags(state) {
     return state.tags;
-  }
-};
-
-const actions = {
-  [FETCH_ARTICLES]({ commit }, params) {
-    commit(FETCH_START);
-    return ArticlesService.query(params.type, params.filters)
-      .then(({ data }) => {
-        commit(FETCH_END, data);
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-  },
-  [FETCH_TAGS]({ commit }) {
-    return TagsService.get()
-      .then(({ data }: any) => {
-        commit(SET_TAGS, data.tags);
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
   }
 };
 
@@ -82,6 +59,5 @@ const mutations = {
 export default {
   state,
   getters,
-  actions,
   mutations
 };
--- Frontend/Vue/src/store/index.ts
+++ Frontend/Vue/src/store/index.ts
@@ -1,12 +1,10 @@
 import { createStore } from "vuex";
 
 import home from "./home.module";
-import article from "./article.module";
 
 export default createStore({
   modules: {
     home,
     // @ts-ignore
-    article,
   }
 });
